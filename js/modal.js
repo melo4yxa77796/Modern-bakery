b7999f16d7c14d078cef915bf57326a2d76c11f0
melo4yxa77796 <g_julija_777@mail.ru>
2025-05-01 12:56:58 +0200

    finished form

--- js/modal.js
+++ js/modal.js
@@ -1,6 +1,6 @@
 const modal = document.querySelector('.backdrop');
 const openModal = document.querySelector(".order-button");
-const closeModal = document.querySelector(".close-button");
+const closeModal = document.querySelector(".modal-btn-close");
 const toggleModal = () => {
     modal.classList.toggle('is-hidden');
 }
